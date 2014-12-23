d4f9a155e11d45079bf90bb805b70c3af8da4120
Harm Pauw <h.pauw@prowareness.nl>
2014-12-23 11:42:13 +0100

    Initial version of selenium chrome node

---
+++ containers/node-chrome/startnode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Run a virtual framebuffer on display :99
+export DISPLAY=:99
+Xvfb $DISPLAY -screen 0 $SCREEN_DIMENSION &
+
+# Run fluxbox as light-weight window manager in the background in order to correctly size windows
+fluxbox -display :99 &
+
+# Run the selenium server as a node
+java -jar /selenium-server-standalone.jar \
+  -role node \
+  -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/wd/register \
+  -browser browserName=chrome,maxInstances=$MAX_INSTANCES
